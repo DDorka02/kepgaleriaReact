3995c9ccf6926c100ce391e99eea44dee6c9a794
DDorka02 <dobszaydorka@gmail.com>
2024-10-10 23:23:58 +0200

    készítés, haladás

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Kiskep from './komponensek/Kiskep.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+          <h1>Képgaléria</h1>
+        </header>
+        <main className="App-main">
+          <div className="fokep">
+            <h3>Kép címe</h3>
+            <div className="kiemelt">
+              <Kiskep></Kiskep>
+            </div>
+            <p>Leírás leírás leírás</p>
+        </div>
+        <div className="galeria">
+        <Kiskep></Kiskep>
+        <Kiskep></Kiskep>
+        <Kiskep></Kiskep>
+        <Kiskep></Kiskep>
+          </div>
+        </main>
+        <footer>
+          <p>Dobszay Dorka</p>
+        </footer>
     </div>
   );
 }
---
+++ src/komponensek/Kiskep.js
@@ -0,0 +1,9 @@
+import "./Kiskep.css"
+
+export default function Kiskep(){
+    return(
+        <div className="kiskep">
+              <img src="../kepek_kicsi/DSC8888.jpeg" alt="" />
+        </div>
+    )
+}
